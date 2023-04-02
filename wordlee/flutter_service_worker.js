1eef12aa16745555a2719c6b68325b0942bc9f57
tferrand <thomas.ferrandini@gmail.com>
2023-04-02 23:05:30 +0000

    Update from https://github.com/tferrand/wordlee/commit/895c28e2400b36257ee7bd075c2f51574c7c82ce

--- wordlee/flutter_service_worker.js
+++ wordlee/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"main.dart.js": "82d38a9bc44cb95680497b9abd42c050",
+"main.dart.js": "6955ebe4987e2afe84b805d8ae6fdc3d",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -35,8 +35,8 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/NOTICES": "6066159c99732c42d71334883df3c0f5",
-"index.html": "18706a90da7634acdc45c81586adcc7a",
-"/": "18706a90da7634acdc45c81586adcc7a",
+"index.html": "ebd0f418fd87e82a098047427d73fdd7",
+"/": "ebd0f418fd87e82a098047427d73fdd7",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
