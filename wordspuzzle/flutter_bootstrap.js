93a2921d63bab882da503746d137562ed18b91e4
tferrand <thomas.ferrandini@gmail.com>
2026-03-29 22:29:29 +0000

    Update from https://github.com/tferrand/wordlee/commit/f9b1e906b1c0316ee036fd0bc42ac8165db4f7b2

--- wordspuzzle/flutter_bootstrap.js
+++ wordspuzzle/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2379419625" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "850803322" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
